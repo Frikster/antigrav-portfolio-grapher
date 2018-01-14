8791bfe02ed28fe4a1789189d030f6b61813bd9f
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2018-01-14 13:00:00 -0800

    fn update

--- app/fn.py
+++ app/fn.py
@@ -6,41 +6,42 @@ import json
 class TicketError(Exception):
     pass
 
-def fn(tickets):
-    if sum(list(tickets.values())) != 100:
-        return None
+class Storage(object):
+    def __init__(self):
+        pass
 
-    datas = []
-    cumulative_dividend = 0.0
+    def fn(self, tickets):
+        if sum(list(tickets.values())) != 100:
+            return None
 
-    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+        datas = []
+        cumulative_dividend = 0.0
 
-    for ticket in tickets:
-        try:
-            (data, meta_data) = ts.get_daily_adjusted(symbol=ticket, outputsize='compact')
-            cumulative_dividend += data["7. dividend amount"].sum()
-            datas.append(data.drop(columns=["6. volume", "7. dividend amount", "8. split coefficient"]) *
-                         (tickets[ticket]/100))
-            # print(cumulative_dividend)
-        except ValueError:
-            raise TicketError(ticket)
+        ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
 
-    data = sum(datas).dropna(axis="rows", how="any")
-    global json_str
-    json_str = data.to_json(orient="index")
-    return json_str
+        for ticket in tickets:
+            try:
+                (data, meta_data) = ts.get_daily_adjusted(symbol=ticket, outputsize='compact')
+                cumulative_dividend += data["7. dividend amount"].sum()
+                datas.append(data.drop(columns=["6. volume", "7. dividend amount", "8. split coefficient"]) *
+                             (tickets[ticket]/100))
+                # print(cumulative_dividend)
+            except ValueError:
+                raise TicketError(ticket)
 
-'''def gn(json_str):
-    if json_str is None:
-        return None
-    return (lambda j: j[-1]['5. adjusted close'] - j[0]['5. adjusted close'])(list(json.loads(json_str).values()))'''
+        data = sum(datas).dropna(axis="rows", how="any")
+        self.json_str = data.to_json(orient="index")
+        return self.json_str
 
-gn = (lambda json_str: None if json_str is None else (
-    lambda j: (
-        j[0]['5. adjusted close'], j[-1]['5. adjusted close'], j[-1]['5. adjusted close'] - j[0]['5. adjusted close']))(
-    list(json.loads(json_str).values())))
+    def gn(self, json_str):
+        if json_str is None: return None
+        return (lambda j: (
+            j[0]['5. adjusted close'], j[-1]['5. adjusted close'], j[-1]['5. adjusted close'] - j[0]['5. adjusted close']))(
+        list(json.loads(json_str).values()))
 
-hn = (lambda: json_str)
+    def hn(self):
+        return self.json_str
+
 
 if __name__ == "__main__":
     import json
