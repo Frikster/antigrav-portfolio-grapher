b9b9e66ade7703de17c3e210cbd74efe17b4001e
Cornelis <dirk.haupt@gmail.com>
2018-01-14 00:30:54 -0800

    can add a fourth ETF

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import ETFForm
 from altair import Chart, X, Y, Axis, Data, DataFormat
@@ -10,11 +10,18 @@ list_data = [10, 20, 30, 20, 15, 30, 45]
 list_name = ['l'+str(x) for x in range(1,8,1)]
 df_list = pd.DataFrame({'data':list_data, 'name':list_name})
 
-@app.route('/')
-@app.route('/index')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    etfs = ["BLEH", "MEH", "HAHA"]
     form = ETFForm()
-    return render_template('base.html', form=form)
+    if form.validate_on_submit():
+        new_etf = request.form['etf']
+        etfs = etfs + [new_etf]
+        print(new_etf)
+        return render_template('base.html', form=form, etfs=etfs) #todo: make different?
+    return render_template('base.html', form=form, etfs=etfs)
 
 @app.route("/line")
 def data_line():
@@ -23,3 +30,11 @@ def data_line():
         Y('data', axis=Axis(title='Value'))
     )
     return chart.to_json()
+
+# @app.route('/handle_data', methods=['POST'])
+# def handle_data():
+#     new_etf = request.form['etf']
+#     etfs = etfs + [new_etf]
+#     print(new_etf)
+#     # process etf
+#     return
