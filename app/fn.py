fb1670e003ca4e2bd0282ac9f2444b69173c7498
Cornelis <dirk.haupt@gmail.com>
2018-01-14 05:46:39 -0800

    first plot working

---
+++ app/fn.py
@@ -0,0 +1,37 @@
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def fn(tickets):
+    if sum(list(tickets.values())) != 100:
+        return None
+
+    datas = []
+    cumulative_dividend = 0.0
+
+    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+
+    for ticket in tickets:
+        (data, meta_data) = ts.get_daily_adjusted(symbol=ticket, outputsize='full')
+        cumulative_dividend += data["7. dividend amount"].sum()
+        datas.append(data.drop(columns=["6. volume", "7. dividend amount", "8. split coefficient"]) *
+                     (tickets[ticket]/100))
+        print(cumulative_dividend)
+
+    data = sum(datas).dropna(axis="rows", how="any")
+    return data.to_json(orient="index")
+
+tickets = {'VAB':100}
+
+# tickets = {'SDIV':25, 'DIV':25, 'ECH':50}
+
+print(fn(tickets))
+
+# (data[~data["7. dividend amount"].isin([0.0000])])["7. dividend amount"].plot()
+#print((data[~data["7. dividend amount"].isin([0.0000])])["7. dividend amount"].sum())
+#print((data[~data["7. dividend amount"].isin([0.0000])])["7. dividend amount"])
+# plt.title('Daily Times Series for the %s stock (Adjusted Close)' % ticket)
+# plt.show()
+
+# plot_json = fn(...) or plot_json
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,51 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import ETFForm
-from altair import Chart, X, Y, Axis, Data, DataFormat
+import altair as alt
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
 import pandas as pd
 
 WIDTH = 600
 HEIGHT = 300
 list_data = [10, 20, 30, 20, 15, 30, 45]
-list_name = ['l'+str(x) for x in range(1,8,1)]
-df_list = pd.DataFrame({'data':list_data, 'name':list_name})
+list_name = ['l'+str(x) for x in range(1, 8, 1)]
+df_list_old = pd.DataFrame({'data': list_data, 'name': list_name})
 
-# Tickers to test: VAB, VFV,
+# Tickers to test: {'SDIV':25, 'DIV':25, 'ECH':50}
+
+
+
+
+def fn(tickets):
+    if sum(list(tickets.values())) != 100:
+        return None
+
+    datas = []
+    cumulative_dividend = 0.0
+
+    ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+
+    for ticket in tickets:
+        (data, meta_data) = ts.get_daily_adjusted(symbol=ticket, outputsize='full')
+        cumulative_dividend += data["7. dividend amount"].sum()
+        datas.append(data.drop(columns=["6. volume", "7. dividend amount", "8. split coefficient"]) *
+                     (tickets[ticket]/100))
+        print(cumulative_dividend)
+
+
+    data = sum(datas).dropna(axis="rows", how="any")
+    print("SUCCESS!")
+    global df_list
+    df_list = pd.DataFrame({"date": list(data["5. adjusted close"].keys()), "price": [k for k in data["5. adjusted close"]]})
+    # return pd.DataFrame(data["5. adjusted close"])
+    return pd.DataFrame({"date": list(data["5. adjusted close"].keys()), "price": [k for k in data["5. adjusted close"]]})
+    # return data.to_json(orient="index")
+fn({'SDIV':100})
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -25,15 +60,27 @@ def index():
     if form.validate_on_submit():
         new_etf = request.form['etf']
         etfs = etfs + [new_etf]
+
         # todo PROCESS NEW ETF
-        return render_template('base.html', form=form, etfs=etfs) #todo: make different?
+        split_even = 100 / len(etfs)
+        tickets = dict(zip(etfs, [split_even] * len(etfs)))
+        print("ABOUT TO COMPUTE!")
+        print(tickets)
+        p = fn(tickets)
+        # df_list = p
+        print(df_list)
+        # todo PROCESS NEW ETF
+
+        return render_template('base.html', form=form, etfs=etfs)  # todo: make different?
     return render_template('base.html', form=form, etfs=etfs)
 
 @app.route("/line")
 def data_line():
-    chart = Chart(data=df_list, height=HEIGHT, width=WIDTH).mark_line().encode(
-        X('name', axis=Axis(title='Sample')),
-        Y('data', axis=Axis(title='Value'))
+    print("data_line")
+    print(df_list)
+    chart = alt.Chart(data=df_list, height=HEIGHT, width=WIDTH).mark_line().encode(
+        x='date:T',
+        y='price:Q',
     )
     return chart.to_json()
 
@@ -44,3 +91,4 @@ def data_line():
 #     print(new_etf)
 #     # process etf
 #     return
+print("done")
